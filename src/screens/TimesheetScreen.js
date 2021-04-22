3eae55e01f0e8819536812c314897b159578dc93
Madeline Petersen <madeline.petersen@ibm.com>
2021-04-22 22:02:02 +0000

    Add button to all tabs

--- src/screens/TimesheetScreen.js
+++ src/screens/TimesheetScreen.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { AntDesign } from '@expo/vector-icons';
 import React from 'react'
@@ -8,7 +8,7 @@ const Tab = createMaterialTopTabNavigator();
 
 function Day({name}) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{name}</Text>
     </View>
   );
@@ -16,22 +16,36 @@ function Day({name}) {
 
 const TimesheetScreen = () => {
   return (
-    <Tab.Navigator
-      tabBarOptions={{
-        activeTintColor: 'black',
-        inactiveTintColor: 'lightgray',
-        labelStyle: { fontSize: 12 }
-      }} 
-    >
-      <Tab.Screen name="SAT">{()=><Day name="Saturday" />}</Tab.Screen>
-      <Tab.Screen name="SUN">{()=><Day name="Sunday" />}</Tab.Screen>
-      <Tab.Screen name="MON">{()=><Day name="Monday" />}</Tab.Screen>
-      <Tab.Screen name="TUE">{()=><Day name="Tuesday" />}</Tab.Screen>
-      <Tab.Screen name="WED">{()=><Day name="Wednesday" />}</Tab.Screen>
-      <Tab.Screen name="THU">{()=><Day name="Thursday" />}</Tab.Screen>
-      <Tab.Screen name="FRI">{()=><Day name="Friday" />}</Tab.Screen>
-    </Tab.Navigator>
+    <View style={styles.container}>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: 'lightgray',
+          labelStyle: { fontSize: 12 }
+        }} 
+      >
+        <Tab.Screen name="SAT">{()=><Day name="Saturday" />}</Tab.Screen>
+        <Tab.Screen name="SUN">{()=><Day name="Sunday" />}</Tab.Screen>
+        <Tab.Screen name="MON">{()=><Day name="Monday" />}</Tab.Screen>
+        <Tab.Screen name="TUE">{()=><Day name="Tuesday" />}</Tab.Screen>
+        <Tab.Screen name="WED">{()=><Day name="Wednesday" />}</Tab.Screen>
+        <Tab.Screen name="THU">{()=><Day name="Thursday" />}</Tab.Screen>
+        <Tab.Screen name="FRI">{()=><Day name="Friday" />}</Tab.Screen>
+      </Tab.Navigator>
+      <AntDesign style={styles.icon} name="pluscircle" size={64} color="green" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%'
+  },
+  icon: {
+    position: 'absolute',
+    bottom: 128,
+    right: 24
+  }
+});
 
 export default TimesheetScreen;
